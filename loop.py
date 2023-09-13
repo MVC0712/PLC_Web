c0f52912fb612148fd2182b15206a6060ca63850
Chien Mong Van (Deformation_Billet) <chien.mv@smcmfg.com.vn>
2023-09-13 08:24:54 +0700

    OK

--- loop.py
+++ loop.py
@@ -1,4 +1,4 @@
-import time 
+import time
 import pymcprotocol
 import mysql.connector
 from datetime import datetime
@@ -6,22 +6,28 @@ from datetime import datetime
 def get_data():
     now = datetime.now()
     date_time = now.strftime("%Y/%m/%d %H:%M:%S")
-    
-    pymc3e = pymcprotocol.Type3E()
-    pymc3e.connect("192.168.1.2", 10000)
-    press_mode = pymc3e.batchread_bitunits(headdevice="M310", readsize=1)
-    all_pump_on = pymc3e.batchread_bitunits(headdevice="M3292", readsize=1)
-    billet_counter = pymc3e.batchread_wordunits(headdevice="D7000", readsize=1)
-    die_name = pymc3e.batchread_wordunits(headdevice="R395", readsize=5)
-    alarm = pymc3e.batchread_wordunits(headdevice="D320", readsize=1)
-    
-    press_mode = press_mode[0]
-    all_pump_on = all_pump_on[0]
-    billet_counter = billet_counter[0]
-    die_name = listToString(die_name)
-    alarm = alarm[0]
+    while True:
+        try:
+            pymc3e = pymcprotocol.Type3E()
+            pymc3e.connect("192.168.1.2", 10000)
+            press_mode = pymc3e.batchread_bitunits(headdevice="M310", readsize=1)
+            all_pump_on = pymc3e.batchread_bitunits(headdevice="M3292", readsize=1)
+            billet_counter = pymc3e.batchread_wordunits(headdevice="D7000", readsize=1)
+            die_name = pymc3e.batchread_wordunits(headdevice="R395", readsize=5)
+            alarm = pymc3e.batchread_wordunits(headdevice="D320", readsize=1)
 
-    return (date_time, press_mode, all_pump_on, billet_counter, die_name, alarm)
+            press_mode = press_mode[0]
+            all_pump_on = all_pump_on[0]
+            billet_counter = billet_counter[0]
+            die_name = listToString(die_name)
+            alarm = alarm[0]
+            machine = 2
+
+            return (date_time, press_mode, all_pump_on, billet_counter, die_name, alarm, machine)
+        except Exception as e:
+            print(e)
+            # pass
+            return (date_time, 0, 0, 0, "Con PLC Error", 0, machine)
 
 def listToString(list):
     str = ""
@@ -40,10 +46,11 @@ def main():
             time.sleep(interval)
             new_data = get_data()
             insert_data_to_log(new_data)
-            sql1 = "SELECT DATE_FORMAT(date_time, '%Y-%m-%d %H:%i') as date_time, press_mode, all_pump_on, billet_counter, die_name, alarm FROM t_plc_web_log ORDER BY date_time DESC LIMIT 1"
+            sql1 = "SELECT DATE_FORMAT(date_time, '%Y-%m-%d %H:%i:%S') as date_time, press_mode, all_pump_on, billet_counter, die_name, alarm, machine FROM t_plc_web_log WHERE machine = 2 ORDER BY date_time DESC LIMIT 1"
             old_data = queryData(sql1)
-            sql2 = "SELECT DATE_FORMAT(date_time, '%Y-%m-%d %H:%i') as date_time, press_mode, all_pump_on, billet_counter, die_name, alarm FROM t_plc_web_log WHERE die_name = '" + str(old_data[4]) + "' AND DATEDIFF('" + date_now + "', '" + str(old_data[0]) + "') = 0 ORDER BY date_time ASC LIMIT 1"
+            sql2 = "SELECT DATE_FORMAT(date_time, '%Y-%m-%d %H:%i:%S') as date_time, press_mode, all_pump_on, billet_counter, die_name, alarm, machine FROM t_plc_web_log WHERE machine = 2 AND die_name = '" + str(old_data[4]) + "' AND DATEDIFF('" + date_now + "', '" + str(old_data[0]) + "') = 0 ORDER BY date_time ASC LIMIT 1"
             start_data = queryData(sql2)
+            # print(start_data)
             # compare_t = compare_tuples(old_data, new_data)
             # if compare_t == False:
             #     insert_data_to_log(new_data)
@@ -51,7 +58,7 @@ def main():
             if compare_m == False:
                 save_data = concatenateData(start_data, new_data)
                 insert_data_to_web(save_data)
-            print([start_data, new_data, save_data])
+                print([start_data, new_data, save_data])
         except Exception as e:
             print(e)
             pass
@@ -77,7 +84,7 @@ def queryData(sql_query):
 def insert_data_to_log(data):
     connection = connect_to_mysql()
     cursor = connection.cursor()
-    insert_query = "INSERT INTO t_plc_web_log (date_time, press_mode, all_pump_on, billet_counter, die_name, alarm) VALUES ('"+ str(data[0]) +"','"+str(data[1])+"','"+str(data[2])+"', '"+str(data[3])+"', '"+str(data[4])+"', '"+str(data[5])+"')"   
+    insert_query = "INSERT INTO t_plc_web_log (date_time, press_mode, all_pump_on, billet_counter, die_name, alarm, machine) VALUES ('"+ str(data[0]) +"','"+str(data[1])+"','"+str(data[2])+"', '"+str(data[3])+"', '"+str(data[4])+"', '"+str(data[5])+"', '"+str(data[6])+"')"   
     cursor.execute(insert_query)
     connection.commit()
     id = cursor.lastrowid
@@ -87,7 +94,7 @@ def insert_data_to_log(data):
 def insert_data_to_web(data):
     connection = connect_to_mysql()
     cursor = connection.cursor()
-    insert_query = "INSERT INTO t_plc_web (start_time, end_time, die_name, billet_quantity) VALUES ('"+ str(data[0]) +"','"+str(data[1])+"','"+str(data[2])+"', '"+str(data[3])+"')"
+    insert_query = "INSERT INTO t_plc_web (start_time, end_time, die_name, billet_quantity, machine) VALUES ('"+ str(data[0]) +"','"+str(data[1])+"','"+str(data[2])+"', '"+str(data[3])+"', '"+str(data[4])+"')"
     cursor.execute(insert_query)
     connection.commit()
     id = cursor.lastrowid
@@ -137,7 +144,7 @@ def compare_tuples(oldData, newData):
     newTuple = newData[1:]
     return oldTuple == newTuple
 
-def concatenateData(oldData, newData):    
+def concatenateData(oldData, newData):
     oldTime = oldData[0]
     oldPressMode = oldData[1]
     start_billet = oldData[3]
@@ -145,10 +152,11 @@ def concatenateData(oldData, newData):
     newPressMode = newData[1]
     new_die_name = newData[4]
     end_billet = newData[3]
+    machine = newData[6]
     if oldPressMode == 0 and newPressMode == 1:
-        return (oldTime, newTime, "Start: "+ new_die_name, end_billet-start_billet)
+        return (oldTime, newTime, "Start: "+ new_die_name, end_billet-start_billet, machine)
     elif oldPressMode == 1 and newPressMode == 0:
-        return (oldTime, newTime, "Stop: "+ new_die_name, end_billet-start_billet)
+        return (oldTime, newTime, "Stop: "+ new_die_name, end_billet-start_billet, machine)
 
 if __name__ == "__main__":
     main()
